fd2c018740dea970c9e5d8f40147a5237aa43810
Rahul Senthil Kumar <rahulsk4087@gmail.com>
2025-01-04 12:57:34 +0530

    feat: concurrently implemented

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import Signin from './signin.js'
-import Signup from './signup.js'
+import Signin from './signin.js';
+import Signup from './signup.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -10,14 +10,21 @@ function App() {
       <Routes>
         {/* Default Route - This will render when no specific route is matched */}
         <Route
-          path="/"
+          path='/'
           element={
-            <div className="d-flex align-items-center justify-content-center vh-100 bg-light">
-              <div className="card text-center shadow" style={{ width: '300px', height: '400px' }}>
-                <div className="card-body d-flex flex-column justify-content-center">
-                  <h1 className="card-title mb-4">Welcome</h1>
-                  <Link to="/signup" className="btn btn-primary mb-2">SignUp</Link>
-                  <Link to="/signin" className="btn btn-primary">SignIn</Link>
+            <div className='d-flex align-items-center justify-content-center vh-100 bg-light'>
+              <div
+                className='card text-center shadow'
+                style={{ width: '300px', height: '400px' }}
+              >
+                <div className='card-body d-flex flex-column justify-content-center'>
+                  <h1 className='card-title mb-4'>Welcome</h1>
+                  <Link to='/signup' className='btn btn-primary mb-2'>
+                    SignUp
+                  </Link>
+                  <Link to='/signin' className='btn btn-primary'>
+                    SignIn
+                  </Link>
                 </div>
               </div>
             </div>
@@ -25,8 +32,8 @@ function App() {
         />
 
         {/* SignIn Route */}
-        <Route path="/signin" element={<Signin />} />
-        <Route path="/signup" element = {<Signup />} />
+        <Route path='/signin' element={<Signin />} />
+        <Route path='/signup' element={<Signup />} />
       </Routes>
     </Router>
   );
