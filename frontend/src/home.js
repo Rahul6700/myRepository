38dfcafc536b45c8452c0c426e7f07a2e508aba3
Rahul Senthil Kumar <rahulsk4087@gmail.com>
2025-01-08 00:19:50 +0530

    feat: logout feat

--- frontend/src/home.js
+++ frontend/src/home.js
@@ -1,12 +1,21 @@
 import { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import Cookies from 'universal-cookie'
 
 export default function Home() {
 
+  const cookies = new Cookies()
+  const navigate = useNavigate();
   const [color, setcolor] = useState('#181818')
 
   const handleClick = () => {
     //send req to backend to change color
   };
+
+  const handleLogout = () =>{
+    cookies.remove("jwt_authorization")
+    navigate('/signin')
+  }
 
   return (
     <div className="d-flex align-items-center justify-content-center vh-100 bg-light">
@@ -23,6 +32,7 @@ export default function Home() {
             }}
           ></div>
           <button className="btn btn-primary mt-3">Change Color</button>
+          <button className="btn btn-danger mt-3" onClick={handleLogout}>Logout</button>
         </div>
       </div>
     </div>
