0db0c054923ad6adce773b64d4b97e9de10a746c
Rahul Senthil Kumar <rahulsk4087@gmail.com>
2025-01-08 00:01:30 +0530

    fix: added content to home

--- frontend/src/home.js
+++ frontend/src/home.js
@@ -1,18 +1,30 @@
-import { v4 as uuidv4 } from 'uuid';
-import { useState } from 'react';
+import { useState } from 'react'
 
 export default function Home() {
-  const [hex, sethex] = useState('#181818');
+
+  const [color, setcolor] = useState('#181818')
 
   const handleClick = () => {
-    sethex(uuidv4().slice(0, 6));
+    //send req to backend to change color
   };
 
   return (
-    <div>
-      <h1>thi5 is the home page</h1>
-      <div style={{ backgroundColor: hex }}></div>
-      <button>Click to change color</button>
+    <div className="d-flex align-items-center justify-content-center vh-100 bg-light">
+      <div className="card text-center shadow" style={{ width: '300px', height: '200px' }}>
+        <div className="card-body d-flex flex-column justify-content-center">
+          <div
+            className="mb-3"
+            id="color-box"
+            style={{
+              width: '100px',
+              height: '100px',
+              margin: '0 auto',
+              backgroundColor: `${color}`,
+            }}
+          ></div>
+          <button className="btn btn-primary mt-3">Change Color</button>
+        </div>
+      </div>
     </div>
   );
 }
